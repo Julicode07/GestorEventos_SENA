3721a7547ef41a7656b34fc4946d365d8c6f8e67
Nicolás G. <ngaleanor06@gmail.com>
2024-08-22 09:23:52 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+const app:Express = express();
+const port = process.env.PORT || 3000;
+
+/*
+// MariaDB session store configuration
+const sessionStore = new MariaDBStore({
+  pool,                                   // using the pool instance from DB connection setup
+  table: 'session',
+  clearExpired: true,                     // automatically clear expired sessions
+  checkExpirationInterval: 900000,        // how frequently expired sessions will be cleared; in milliseconds
+}) as unknown as session.Store;           // Casting to session.Store to satisfy TypeScript.
+*/
+
+app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public_views')));
+
+/*
+// Session initializing and type declarations.
+app.use(session({
+  name: 'ssid_dont_share',
+  secret: process.env.SESSION_SECRET as string,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 3
+  }
+}));
+
+
+type UserSession = {
+  phone: number;
+  role: UserRole
+};
+
+declare module "express-session" { // Augment express-session with a custom SessionData object
+  interface SessionData {
+    user: UserSession;
+  }
+}
+*/
+
+// Some security options.
+app.set('x-powered-by', false);
+app.use(helmet({ contentSecurityPolicy: false }));
+
+//app.use("/api/accounts", AccountsRouter);
+/*
+app.use("/api/web-profiles", WebProfileRouter);
+app.use("/api/categories", CategoriesRouter);
+app.use("/api/products", ProductsRouter);
+app.use("/api/auth", LogInRouter);
+app.use("/images/", ImagesRouter);
+*/
+
+app.get("/*", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public_views', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// createSchemas();
