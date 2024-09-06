62b5e8862a4b1985306fd5975ac581ad3c215abd
Felipe Alzate Gil <felipegil444@gmail.com>
2024-09-06 17:10:22 -0500

    logout

--- src/modules/MainPage/UserDropdown.jsx
+++ src/modules/MainPage/UserDropdown.jsx
@@ -8,6 +8,11 @@ import {
 import { Link } from "react-router-dom";
 
 const UserDropdown = ({ role, profileLink, logoutLink }) => {
+  const logOut = async () => {
+    await fetch(`${import.meta.env.VITE_API_URL}/api/auth/logout`);
+    window.location.href = "/";
+  };
+
   return (
     <Dropdown placement="bottom-start">
       <DropdownTrigger>
@@ -46,7 +51,11 @@ const UserDropdown = ({ role, profileLink, logoutLink }) => {
           color="danger"
           className="flex bg-red-100 mt-2"
         >
-          <div className="flex items-center justify-between">
+          <button
+            onClick={logOut}
+            type="submit"
+            className="flex items-center justify-between"
+          >
             <span>Cerrar sesión</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -56,7 +65,7 @@ const UserDropdown = ({ role, profileLink, logoutLink }) => {
             >
               <path d="M5 2H19C19.5523 2 20 2.44772 20 3V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2ZM9 11V8L4 12L9 16V13H15V11H9Z"></path>
             </svg>
-          </div>
+          </button>
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
