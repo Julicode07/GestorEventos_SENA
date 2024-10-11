5d1005a69955cebd3e65e0cb084cd8fc83376616
ArmyNicolasG <ngaleanor06@gmail.com>
2024-10-11 13:07:02 -0500

    added instructor middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,3 +9,25 @@ export async function isAuthenticated(req:Request, res:Response, next:NextFuncti
       return res.status(401).json({ message: 'No autorizado' });
     }
 }
+
+
+export async function isInstructor(req:Request, res:Response, next:NextFunction) {
+    if (req.session && req.session.user?.role == "Instructor") {
+      // User is authenticated
+      return next(); // Move to the next middleware/route handler
+    } else {
+      // User is not authenticated
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+}
+
+
+export async function isCoordinator(req:Request, res:Response, next:NextFunction) {
+    if (req.session && req.session.user?.role == "Coordinador") {
+      // User is authenticated
+      return next(); // Move to the next middleware/route handler
+    } else {
+      // User is not authenticated
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+}
