b16836c11b169a16b5f7691b48efdac844420165
ArmyNicolasG <ngaleanor06@gmail.com>
2024-11-05 10:10:13 -0500

    added 404 page

--- index.ts
+++ index.ts
@@ -72,7 +72,76 @@ app.get("/404.html", (req:Request, res:Response) => {
 app.get("/*", (_req: Request, res: Response) => {
     const viewFile = path.join(__dirname, 'public_views', 'index.html');
     if (fs.existsSync(viewFile)) res.sendFile(viewFile);
-    else res.sendFile(path.join(__dirname, 'public_views', '404.html'));
+    else {
+      return res.status(403).send(`<!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Estamos en mantenimiento</title>
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                    font-family: Arial, sans-serif;
+                    background-color: #f0f0f0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #111827;
+                }
+        
+                .container {
+                    text-align: center;
+                    background-color: #ffffff;
+                    padding: 40px;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                }
+        
+                h1 {
+                    font-size: 24px;
+                    color: #333333;
+                    margin-bottom: 20px;
+                }
+        
+                p {
+                    font-size: 18px;
+                    color: #666666;
+                    margin-bottom: 20px;
+                }
+        
+                .button {
+                    margin-top: 45px;
+                    padding: 10px 20px;
+                    background-color: #007BFF;
+                    color: white;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    font-size: 16px;
+                }
+        
+                .button:hover {
+                    background-color: #0056b3;
+                }
+    
+                .emoji {
+                  font-size: 32px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1 class="emoji">🚧👷🏻‍♂️</h1>
+                <h1>Estamos en mantenimiento</h1>
+                <p>Vuelve pronto para seguir comprando.</p>
+                <a href="https://google.com" class="button">Salir</a>
+            </div>
+        </body>
+        </html>
+        `);
+    }
 });
 
 app.listen(port, () => {
