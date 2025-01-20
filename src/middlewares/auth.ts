8def7b33f0a0fc971d0cca77f91e8d0ebf024169
Felipe <felipegil444@gmail.com>
2025-01-20 15:55:26 -0500

    commenting the auth of coordinador

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -22,12 +22,12 @@ export async function isInstructor(req:Request, res:Response, next:NextFunction)
 }
 
 
-export async function isCoordinator(req:Request, res:Response, next:NextFunction) {
-    if (req.session && req.session.user?.role == "Coordinador") {
-      // User is authenticated
-      return next(); // Move to the next middleware/route handler
-    } else {
-      // User is not authenticated
-      return res.status(401).json({ message: 'No autorizado' });
-    }
-}
+// export async function isCoordinator(req:Request, res:Response, next:NextFunction) {
+//     if (req.session && req.session.user?.role == "Coordinador") {
+//       // User is authenticated
+//       return next(); // Move to the next middleware/route handler
+//     } else {
+//       // User is not authenticated
+//       return res.status(401).json({ message: 'No autorizado' });
+//     }
+// }
