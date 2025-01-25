adf1b046614b2de03696a407206ea36eb40b6629
Felipe <felipegil444@gmail.com>
2025-01-24 19:27:49 -0500

    Endpoint to get all the organizers

--- src/modules/Admin/coordinador/organizadores/Organizadores.jsx
+++ src/modules/Admin/coordinador/organizadores/Organizadores.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   Table,
   TableHeader,
@@ -6,74 +6,28 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  Input,
   Button,
   DropdownTrigger,
   Dropdown,
   DropdownMenu,
   DropdownItem,
-  Chip,
-  User,
-  Pagination,
   Breadcrumbs,
   BreadcrumbItem,
 } from "@nextui-org/react";
 
-export const columns = [
-  { name: "ID", uid: "id", sortable: true },
-  { name: "NOMBRE DEL EVENTO", uid: "event_name", sortable: true },
-  { name: "NOMBRE DEL ORGANIZADOR", uid: "organizer_name", sortable: true },
-  { name: "ROLE", uid: "role", sortable: true },
-  { name: "EMAIL", uid: "email" },
-  { name: "DIRECCION", uid: "address", },
-  { name: "ACTIONS", uid: "actions" },
-];
-
-export const organizers = [
-  {
-    id: 1,
-    event_name: "instr",
-    organizer_name: "Tony Reichert",
-    role: "Externo",
-    email: "tony.reichert@example.com",
-    address: "Calle 123 # 456",
-  }, {
-    id: 2,
-    event_name: "Dia de la mujer",
-    organizer_name: "Tony Reichert",
-    role: "Externo",
-    email: "tony.reichert@example.com",
-    address: "Calle 123 # 456",
-  },
-];
-
-export function capitalize(s) {
-  return s ? s.charAt(0).toUpperCase() + s.slice(1).toLowerCase() : "";
-}
-
 import { VerticalDotsIcon } from "@modules/Admin/components/VerticalDotsIcon";
-import { SearchIcon } from "@modules/Admin/components/SearchIcon";
-import { ChevronDownIcon } from "@modules/Admin/components/ChevronDownIcon";
-import ModalOrganizador from "./ModalOrganizador";
-
-const statusColorMap = {
-  active: "success",
-  paused: "danger",
-  vacation: "warning",
-};
-
-const INITIAL_VISIBLE_COLUMNS = [
-  "id",
-  "event_name",
-  "organizer_name",
-  "role",
-  "email",
-  "address",
-  "actions",];
+const ModalOrganizador = React.lazy(() => import("./ModalOrganizador"));
+const TopContent = React.lazy(() => import("./../../components/TopContent"));
+const BottomContent = React.lazy(() =>
+  import("./../../components/BottonContent")
+);
+import { columns, INITIAL_VISIBLE_COLUMNS, capitalize } from "./utils/utils";
 
 export default function App() {
   const [filterValue, setFilterValue] = React.useState("");
-  const [visibleColumns, setVisibleColumns] = React.useState(new Set(INITIAL_VISIBLE_COLUMNS));
+  const [visibleColumns, setVisibleColumns] = React.useState(
+    new Set(INITIAL_VISIBLE_COLUMNS)
+  );
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [sortDescriptor, setSortDescriptor] = React.useState({
     column: "age",
@@ -83,29 +37,77 @@ export default function App() {
 
   const hasSearchFilter = Boolean(filterValue);
 
+  //Get organizers
+  const [getOrganizers, setOrganizers] = useState([]);
+
+  const getData = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/organizers/all`
+      );
+      const data = await response.json();
+      setOrganizers(data);
+    } catch (err) {
+      console.error("No se pudo traer la data", err);
+    }
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+  //
+
   const headerColumns = React.useMemo(() => {
     if (visibleColumns === "all") return columns;
 
-    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+    return columns.filter((column) =>
+      Array.from(visibleColumns).includes(column.uid)
+    );
   }, [visibleColumns]);
 
   const filteredItems = React.useMemo(() => {
-    let filteredOrganizers = [...organizers];
+    let filteredOrganizers = [...getOrganizers];
 
     if (hasSearchFilter) {
-      filteredOrganizers = filteredOrganizers.filter((organizer) =>
-        organizer.event_name.toLowerCase().includes(filterValue.toLowerCase()) ||
-        organizer.organizer_name.toLowerCase().includes(filterValue.toLowerCase()) ||
-        organizer.role.toLowerCase().includes(filterValue.toLowerCase()) ||
-        organizer.email.toLowerCase().includes(filterValue.toLowerCase()) ||
-        organizer.address.toLowerCase().includes(filterValue.toLowerCase())
+      filteredOrganizers = filteredOrganizers.filter(
+        (organizer) =>
+          organizer.event_name
+            .toLowerCase()
+            .includes(filterValue.toLowerCase()) ||
+          organizer.organizer_name
+            .toLowerCase()
+            .includes(filterValue.toLowerCase()) ||
+          organizer.role.toLowerCase().includes(filterValue.toLowerCase()) ||
+          organizer.email.toLowerCase().includes(filterValue.toLowerCase()) ||
+          organizer.address.toLowerCase().includes(filterValue.toLowerCase())
       );
     }
 
     return filteredOrganizers;
-  }, [filterValue, hasSearchFilter]);
+  }, [filterValue, hasSearchFilter, getOrganizers]);
 
   const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  //Top Content
+  const onRowsPerPageChange = React.useCallback((e) => {
+    setRowsPerPage(Number(e.target.value));
+    setPage(1);
+  }, []);
+
+  const onSearchChange = React.useCallback((value) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const onClear = React.useCallback(() => {
+    setFilterValue("");
+    setPage(1);
+  }, []);
+  //
 
   const items = React.useMemo(() => {
     const start = (page - 1) * rowsPerPage;
@@ -128,29 +130,6 @@ export default function App() {
     const cellValue = user[columnKey];
 
     switch (columnKey) {
-      case "name":
-        return (
-          <User
-            avatarProps={{ radius: "lg", src: user.avatar }}
-            description={user.email}
-            name={cellValue}
-          >
-            {user.email}
-          </User>
-        );
-      case "role":
-        return (
-          <div className="flex flex-col">
-            <p className="text-bold text-small capitalize">{cellValue}</p>
-            <p className="text-bold text-tiny capitalize text-default-400">{user.team}</p>
-          </div>
-        );
-      case "status":
-        return (
-          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
-            {cellValue}
-          </Chip>
-        );
       case "actions":
         return (
           <div className="relative flex justify-end items-center gap-2">
@@ -173,102 +152,6 @@ export default function App() {
     }
   }, []);
 
-  const onRowsPerPageChange = React.useCallback((e) => {
-    setRowsPerPage(Number(e.target.value));
-    setPage(1);
-  }, []);
-
-  const onSearchChange = React.useCallback((value) => {
-    if (value) {
-      setFilterValue(value);
-      setPage(1);
-    } else {
-      setFilterValue("");
-    }
-  }, []);
-
-  const onClear = React.useCallback(() => {
-    setFilterValue("");
-    setPage(1);
-  }, []);
-
-  const topContent = React.useMemo(() => {
-    return (
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between gap-3 items-end">
-          <Input
-            isClearable
-            className="w-full sm:max-w-[44%]"
-            placeholder="Buscar organizador"
-            startContent={<SearchIcon />}
-            value={filterValue}
-            onClear={() => onClear()}
-            onValueChange={onSearchChange}
-          />
-          <div className="flex gap-3">
-            <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
-                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
-                  Columnas
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                disallowEmptySelection
-                aria-label="Table Columns"
-                closeOnSelect={false}
-                selectedKeys={visibleColumns}
-                selectionMode="multiple"
-                onSelectionChange={setVisibleColumns}
-              >
-                {columns.map((column) => (
-                  <DropdownItem key={column.uid} className="capitalize">
-                    {capitalize(column.name)}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-            <ModalOrganizador />
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <span className="text-default-400 text-small">Total {organizers.length} organizadores</span>
-          <label className="flex items-center text-default-400 text-small">
-            Rows per page:
-            <select
-              className="bg-transparent outline-none text-default-400 text-small"
-              onChange={onRowsPerPageChange}
-            >
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-            </select>
-          </label>
-        </div>
-      </div>
-    );
-  }, [
-    filterValue,
-    onClear,
-    visibleColumns,
-    onRowsPerPageChange,
-    onSearchChange,
-  ]);
-
-  const bottomContent = React.useMemo(() => {
-    return (
-      <div className="py-0 px-2 flex justify-center items-center">
-        <Pagination
-          showControls
-          isCompact
-          showShadow
-          page={page}
-          total={pages}
-          onChange={(page) => setPage(page)}
-        />
-      </div>
-    );
-  }, [page, pages]);
-
   return (
     <main className="flex flex-col gap-2">
       <div>
@@ -282,13 +165,29 @@ export default function App() {
       <section>
         <Table
           aria-label="Example table with custom cells, pagination and sorting"
-          bottomContent={bottomContent}
+          bottomContent={
+            <BottomContent page={page} pages={pages} setPage={setPage} />
+          }
           bottomContentPlacement="outside"
           classNames={{
             wrapper: "max-h-[382px]",
           }}
           sortDescriptor={sortDescriptor}
-          topContent={topContent}
+          topContent={
+            <TopContent
+              filterValue={filterValue}
+              onClear={onClear}
+              onRowsPerPageChange={onRowsPerPageChange}
+              onSearchChange={onSearchChange}
+              rowsPerPage={rowsPerPage}
+              visibleColumns={visibleColumns}
+              module={getOrganizers}
+              setVisibleColumns={setVisibleColumns}
+              columns={columns}
+              capitalize={capitalize}
+              ModuleModal={ModalOrganizador}
+            />
+          }
           topContentPlacement="outside"
           onSortChange={setSortDescriptor}
         >
@@ -303,10 +202,15 @@ export default function App() {
               </TableColumn>
             )}
           </TableHeader>
-          <TableBody emptyContent={"No se encontraron organizadores"} items={sortedItems}>
+          <TableBody
+            emptyContent={"No se encontraron organizadores"}
+            items={sortedItems}
+          >
             {(item) => (
-              <TableRow key={item.id}>
-                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+              <TableRow key={item.id_organizers}>
+                {(columnKey) => (
+                  <TableCell>{renderCell(item, columnKey)}</TableCell>
+                )}
               </TableRow>
             )}
           </TableBody>
@@ -315,4 +219,3 @@ export default function App() {
     </main>
   );
 }
-
---
+++ src/modules/Admin/coordinador/organizadores/utils/utils.js
@@ -0,0 +1,24 @@
+const columns = [
+  { name: "ID", uid: "id_organizers", sortable: true },
+  { name: "NOMBRE DEL EVENTO", uid: "sub_event_name", sortable: true },
+  { name: "NOMBRE DEL ORGANIZADOR", uid: "organizer_name", sortable: true },
+  { name: "ROLE", uid: "rol", sortable: true },
+  { name: "EMAIL", uid: "email" },
+  { name: "DIRECCION", uid: "address" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+const INITIAL_VISIBLE_COLUMNS = [
+  "id_organizers",
+  "sub_event_name",
+  "organizer_name",
+  "rol",
+  "email",
+  "address",
+  "actions",
+];
+
+function capitalize(s) {
+  return s ? s.charAt(0).toUpperCase() + s.slice(1).toLowerCase() : "";
+}
+export { columns, INITIAL_VISIBLE_COLUMNS, capitalize };
