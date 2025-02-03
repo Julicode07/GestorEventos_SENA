62180c9b9285d53b4728c778e3147dddc0df579f
Felipe <felipegil444@gmail.com>
2025-02-03 13:42:08 -0500

    Fixing the query in subEvents_has_spaces model

--- src/repositories/subEvents_has_spaces/models.ts
+++ src/repositories/subEvents_has_spaces/models.ts
@@ -10,13 +10,13 @@ export interface ISubEventHasSpace {
 export async function createSubEventHasSpaceSchema(): Promise<Number> {
   const connection: PoolConnection = await getConnection(pool);
   try {
-    await connection.query(`CREATE TABLE IF NOS EXISTS sub_events_has_spaces (
-      id_sub_event_has_space INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-      id_sub_event INT NOT NULL,
-      id_space INT NOT NULL,
-      CONSTRAINT fk_sub_events_has_spaces FOREIGN KEY (id_sub_event) REFERENCES sub_events(id_sub_event),
-      CONSTRAINT fk_spaces_has_sub_events FOREIGN KEY (id_space) REFERENCES spaces(id_space)
-    )`);
+    await connection.query(`CREATE TABLE IF NOT EXISTS sub_events_has_spaces (
+  id_sub_event_has_space INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_sub_event INT NOT NULL,
+  id_space INT NOT NULL,
+  CONSTRAINT fk_sub_events_has_spaces FOREIGN KEY (id_sub_event) REFERENCES sub_events(id_sub_event),
+  CONSTRAINT fk_spaces_has_sub_events FOREIGN KEY (id_space) REFERENCES spaces(id_space)
+)`);
 
     console.log(
       `[subevents_has_spaces repository - models]: CREATED subevents_has_spaces SCHEMA.`
