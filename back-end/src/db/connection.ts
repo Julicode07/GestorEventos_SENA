79fc20b72f7154ec82cd3fe1865777f387e1973c
JuliCode07 <Julicode07@gmail.com>
2025-02-21 16:28:21 -0500

    Fix enviroments

--- back-end/src/db/connection.ts
+++ back-end/src/db/connection.ts
@@ -1,24 +1,27 @@
-import mariadb, { PoolConfig, Pool } from 'mariadb';
-import dotenv from "dotenv";
+import mysql, { PoolOptions, Pool } from 'mysql2/promise';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const poolConfig:PoolConfig = {
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectionLimit: 50
+const poolConfig: PoolOptions = {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 50,
+  queueLimit: 0,
 };
 
-const pool: Pool = mariadb.createPool(poolConfig);
+const pool: Pool = mysql.createPool(poolConfig);
 
-async function getConnection(pool: Pool) {
+async function getConnection() {
   try {
     const connection = await pool.getConnection();
     return connection;
   } catch (err) {
-    console.error('Error getting a connection from the pool:', err);
+    console.error('❌ Error getting a connection from the pool:', err);
     throw err;
   }
 }
