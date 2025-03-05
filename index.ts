5638c6fe9c0f108d088be9c2c318b9412282b8cd
JuliCode07 <Julicode07@gmail.com>
2025-03-04 22:11:02 -0500

    Create SESSION_SECRET

--- index.ts
+++ index.ts
@@ -15,61 +15,63 @@ import SpacesRouter from "./src/routes/spaces";
 import InventoryRouter from "./src/routes/inventory";
 import SubEventsRouter from "./src/routes/subEvents";
 import OrganizersRouter from "./src/routes/organizers";
-//import SubEventsHasSpacesRouter from "./src/routes/subEvents_has_spaces";
 import compression from "compression";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const sessionSecret = process.env.SESSION_SECRET;
 
+if (!sessionSecret) {
+  console.error("❌ ERROR: SESSION_SECRET no está definido en .env");
+  process.exit(1);
+}
+
+// Middleware
 app.use(compression());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public_views")));
+app.use(helmet()); // Seguridad
 
-// Some security options.
-app.set("x-powered-by", false);
-app.use(helmet({ contentSecurityPolicy: false }));
-
-// MariaDB session store configuration
+// Configuración de la sesión con MariaDB
 const sessionStore = new MariaDBStore({
-  pool, // using the pool instance from DB connection setup
+  pool,
   table: "session",
-  clearExpired: true, // automatically clear expired sessions
-  checkExpirationInterval: 900000, // how frequently expired sessions will be cleared; in milliseconds
-}) as unknown as session.Store; // Casting to session.Store to satisfy TypeScript.
+  clearExpired: true,
+  checkExpirationInterval: 900000,
+}) as unknown as session.Store;
 
-// Session initializing and type declarations.
 app.use(
   session({
     name: "ssid_dont_share",
-    secret: process.env.SESSION_SECRET as string,
+    secret: sessionSecret,
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
     cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24 * 3,
     },
   })
 );
 
-type UserSession = {
-  document: number | null;
-  role: string | null;
-  id_user: number | null;
-  name: string | null;
-  last_name: string | null;
-};
-
+// Definición de la interfaz para los datos de sesión
 declare module "express-session" {
-  // Augment express-session with a custom SessionData object
   interface SessionData {
-    user: UserSession;
+    user: {
+      document: number | null;
+      role: string | null;
+      id_user: number | null;
+      name: string | null;
+      last_name: string | null;
+    };
   }
 }
 
-// ROUTES.
+// Rutas
 app.use("/api/users/", UsersRouter);
 app.use("/api/auth/", AuthRouter);
 app.use("/api/events/", EventsRouter);
@@ -77,90 +79,80 @@ app.use("/api/spaces/", SpacesRouter);
 app.use("/api/inventory/", InventoryRouter);
 app.use("/api/subEvents/", SubEventsRouter);
 app.use("/api/organizers/", OrganizersRouter);
-//app.use("/api/subEvents_has_spaces/", SubEventsHasSpacesRouter);
 
+// Manejo de rutas estáticas
 app.get("/404.html", (req: Request, res: Response) => {
   const viewFile = path.join(__dirname, "public_views", "404.html");
-  if (fs.existsSync(viewFile)) res.sendFile(viewFile);
-  else res.redirect(path.join(__dirname, "public_views", "index.html"));
+  res.sendFile(fs.existsSync(viewFile) ? viewFile : path.join(__dirname, "public_views", "index.html"));
 });
+
 app.get("/*", (_req: Request, res: Response) => {
   const viewFile = path.join(__dirname, "public_views", "index.html");
-  if (fs.existsSync(viewFile)) res.sendFile(viewFile);
-  else {
-    return res.status(403).send(`<!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Estamos en mantenimiento</title>
-            <style>
-                body {
-                    margin: 0;
-                    padding: 0;
-                    font-family: Arial, sans-serif;
-                    background-color: #f0f0f0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    background-color: #111827;
-                }
-        
-                .container {
-                    text-align: center;
-                    background-color: #ffffff;
-                    padding: 40px;
-                    border-radius: 10px;
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                }
-        
-                h1 {
-                    font-size: 24px;
-                    color: #333333;
-                    margin-bottom: 20px;
-                }
-        
-                p {
-                    font-size: 18px;
-                    color: #666666;
-                    margin-bottom: 20px;
-                }
-        
-                .button {
-                    margin-top: 45px;
-                    padding: 10px 20px;
-                    background-color: #007BFF;
-                    color: white;
-                    text-decoration: none;
-                    border-radius: 5px;
-                    font-size: 16px;
-                }
-        
-                .button:hover {
-                    background-color: #0056b3;
-                }
-    
-                .emoji {
-                  font-size: 32px;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <h1 class="emoji">🚧👷🏻‍♂️</h1>
-                <h1>Estamos en mantenimiento</h1>
-                <p>Vuelve pronto para seguir agendando.</p>
-                <a href="https://google.com" class="button">Salir</a>
-            </div>
-        </body>
-        </html>
-        `);
+  if (fs.existsSync(viewFile)) {
+    res.sendFile(viewFile);
+  } else {
+    res.status(403).send(`
+      <!DOCTYPE html>
+      <html lang="es">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Estamos en mantenimiento</title>
+          <style>
+              body {
+                  margin: 0;
+                  padding: 0;
+                  font-family: Arial, sans-serif;
+                  background-color: #111827;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100vh;
+              }
+              .container {
+                  text-align: center;
+                  background-color: #ffffff;
+                  padding: 40px;
+                  border-radius: 10px;
+                  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  font-size: 24px;
+                  color: #333;
+              }
+              p {
+                  font-size: 18px;
+                  color: #666;
+              }
+              .button {
+                  margin-top: 20px;
+                  padding: 10px 20px;
+                  background-color: #007BFF;
+                  color: white;
+                  text-decoration: none;
+                  border-radius: 5px;
+              }
+              .button:hover {
+                  background-color: #0056b3;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>🚧 Estamos en mantenimiento</h1>
+              <p>Vuelve pronto para seguir agendando.</p>
+              <a href="https://google.com" class="button">Salir</a>
+          </div>
+      </body>
+      </html>
+    `);
   }
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
+// Crear esquemas en la base de datos
 createSchemas();
