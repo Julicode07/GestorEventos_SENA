c2fb7ef59821e94b8b370c90b646461bce044b30
JuliCode07 <Julicode07@gmail.com>
2025-03-05 09:39:14 -0500

    Change front end url

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ if (!sessionSecret) {
 app.use(compression());
 app.use(
   cors({
-    origin: ["https://gestro-eventos-sena.netlify.app"],
+    origin: ["https://julicode07.github.io/GestorEventos_SENA"],
     credentials: true,
   })
 );
