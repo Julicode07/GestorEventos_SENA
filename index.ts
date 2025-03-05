586794df4cb1f1c7d45d9a47bb36628e1e932f56
JuliCode07 <Julicode07@gmail.com>
2025-03-05 08:21:59 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -34,7 +34,6 @@ app.use(
   cors({
     origin: ["https://gestro-eventos-sena.netlify.app"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
